e1ca9133de909aacf90d1bad8f492305948e613d
Elida <blubber3210@gmail.com>
2025-11-20 11:34:57 +0100

    init

---
+++ model.js
@@ -0,0 +1,36 @@
+
+const model = {
+    app: {
+        currentPage: 'homePage',
+
+        pages: [
+            'homePage',
+            'jsPage',
+            'cSharpPage',
+            'updateNotesPage',
+        ],
+    },
+
+    viewState: {
+
+    },
+
+    data: {
+        text: '',
+    }
+}
+
+
+
+let content = '';
+
+// Operator examples
+
+let num1 = 0;
+let num2 = 0;
+let selectedOperator = '';
+let operatorResult = 0;
+
+// string examples
+let stringExample = `This is a string example, with some text. I don't know what it should say.`;
+
